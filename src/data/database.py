65895f2e0bad74adbcb2db1ab9f63b5c76560d39
Thijs <1051036@hr.nl>
2025-06-03 16:11:27 +0200

    Added: Database

--- src/data/database.py
+++ src/data/database.py
@@ -1,28 +1,28 @@
 import sqlite3
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from visual.text_colors import TextColors
+
+t = TextColors
 
 DB_FILE = "urban_mobility.db"
-
-
-def get_connection():
-    """Returns a connection to the SQLite database."""
-    return sqlite3.connect(DB_FILE)
 
 
 def initialize_database():
     """Initializes the database and creates necessary tables."""
     if not os.path.exists(DB_FILE):
-        print("[INFO] Creating database...")
-        with get_connection() as conn:
+        print(f"{t.blue}[INFO] Creating database...{t.end}")
+        with sqlite3.connect(DB_FILE) as conn:
             cursor = conn.cursor()
             create_users_table(cursor)
             create_travellers_table(cursor)
             create_scooters_table(cursor)
-            create_logs_table(cursor)
             conn.commit()
-            print("[INFO] Database initialized.")
+            print(f"{t.green}[INFO] Database initialized.{t.end}")
     else:
-        print("[INFO] Database already exists.")
+        print(f"{t.red}[INFO] Database already exists.{t.end}")
 
 
 def create_users_table(cursor):
@@ -43,18 +43,18 @@ def create_travellers_table(cursor):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS travellers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            first_name TEXT,
-            last_name TEXT,
-            birthday TEXT,
-            gender TEXT,
-            street TEXT,
-            house_number TEXT,
-            zip_code TEXT,
-            city TEXT,
-            email TEXT,
-            mobile TEXT,
-            license_number TEXT,
-            registration_date TEXT
+            first_name VARCHAR(30),
+            last_name VARCHAR(30),
+            birthday DATE,
+            gender CHAR,
+            street VARCHAR(30),
+            house_number INT,
+            zip_code VARCHAR(6),
+            city VARCHAR(30),
+            email VARCHAR(50),
+            mobile CHAR(14),
+            license_number CHAR(9),
+            registration_date DATE
         );
     """)
 
@@ -63,33 +63,17 @@ def create_scooters_table(cursor):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS scooters (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            brand TEXT,
-            model TEXT,
-            serial_number TEXT UNIQUE,
-            top_speed REAL,
-            battery_capacity REAL,
-            soc INTEGER,
-            soc_range_min INTEGER,
-            soc_range_max INTEGER,
-            latitude REAL,
-            longitude REAL,
-            out_of_service INTEGER,
-            mileage REAL,
-            last_maintenance TEXT,
-            in_service_date TEXT
+            brand VARCHAR(30),
+            model VARCHAR(30),
+            serial_number VARCHAR(30) UNIQUE,
+            top_speed INT,
+            battery_capacity INT,
+            soc DECIMAL(5,2),
+            target_range_soc VARCHAR(20),
+            location VARCHAR(50),
+            out_of_service INT,
+            mileage DECIMAL(7,1),
+            last_maintenance DATE
         );
     """)
 
-
-def create_logs_table(cursor):
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date TEXT,
-            time TEXT,
-            username TEXT,
-            activity TEXT,
-            extra_info TEXT,
-            suspicious INTEGER
-        );
-    """)
