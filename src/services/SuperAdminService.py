180422ca8d1ca3b4bb8cb74292760eb253609535
yassyass2 <yassinabde@outlook.com>
2025-06-04 22:54:37 +0200

    overview geeft lijst met user objects

--- src/services/SuperAdminService.py
+++ src/services/SuperAdminService.py
@@ -2,8 +2,8 @@ import bcrypt
 import os
 import sqlite3
 import sys
-from src.models import Session
-from src.models import User
+from src.models.Session import Session
+from src.models.User import User
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -14,12 +14,17 @@ class ServiceEngineerService():
     def __init__(self, session: Session):
         self.session: Session = session
 
-    def user_overview(self):
+    def user_overview(self) -> list[User]:
         if (not self.session.is_valid() or self.session.role not in ["super_admin, system_admin"]):
             print("invalid session")
             return
         with sqlite3.connect(DB_FILE) as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT id, username, password_hash, role, first_name, last_name, registration_date  FROM Users")
-            all_users = cursor.fetchall()
-        return all_users
+            cursor.execute(
+                "SELECT id, username, role, first_name, last_name, registration_date  FROM Users")
+            rows = cursor.fetchall()
+
+        users = [User(id=row[0], username=row[1], password_hash=None,
+                 role=row[2], first_name=row[3], last_name=row[4],
+                 registration_date=row[5]) for row in rows]
+        return users
